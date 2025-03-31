154e0d5fe8e63599b4929686d745756eeb4af406
chataignault <leonchat@orange.fr>
2025-03-31 11:42:12 +0100

    clean python module imports

--- a_star/__init__.py
+++ a_star/__init__.py
@@ -1,7 +1,2 @@
-# from a_star._lib_name import sum_as_string
-# from a_star.utils import sum_as_string
-# from a_star.utils import PyState
 
-# state = PyState([1,2,3], 3.)
-
-from utils import *
+from utils import PyState
